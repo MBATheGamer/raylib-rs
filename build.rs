846c9e78d3101072c8bbc52275117dea6cc232f7
MBATheGamer <mbathegamer@gmail.com>
2025-01-01 17:15:14 +0100

    Add raylib library to build script (Linux x64)

--- build.rs
+++ build.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use std::env;
+
+fn main() {
+  println!("cargo:rustc-link-search=./lib");
+
+  let target = env::var("TARGET").expect("TARGET environment variable not set.");
+
+  match target.as_str() {
+    "x86_64-unknown-linux-gnu" => println!("cargo:rustc-link-lib=raylib_x64"),
+    _ => println!("cargo:warning=Unsupported target architecture"),
+  }
+}

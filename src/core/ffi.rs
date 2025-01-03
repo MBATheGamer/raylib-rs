f921f641bb4514ebf7cfd35290b43f50766f2f1c
MBATheGamer <mbathegamer@gmail.com>
2025-01-03 18:43:38 +0100

    Add `BeginDrawing` to `core/ffi` module

--- src/core/ffi.rs
+++ src/core/ffi.rs
@@ -1,5 +1,9 @@
 unsafe extern "C" {
+  // Window-related functions
   pub unsafe fn InitWindow(width: i32, height: i32, title: *const i8);
   pub unsafe fn CloseWindow();
   pub unsafe fn WindowShouldClose() -> bool;
+
+  // Drawing-related functions
+  pub unsafe fn BeginDrawing();
 }

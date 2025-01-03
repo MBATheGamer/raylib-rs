61b84a2c49788b749e854ea54585a65128f2450e
MBATheGamer <mbathegamer@gmail.com>
2025-01-03 22:07:43 +0100

    Create `RAYWHITE` object inside `consts/colors` module

---
+++ src/consts/colors.rs
@@ -0,0 +1,8 @@
+use crate::structs::Color;
+
+pub const RAYWHITE: Color = Color {
+  red: 245,
+  green: 245,
+  blue: 245,
+  alpha: 255,
+};
--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -1,1 +1,3 @@
 mod colors;
+
+pub use colors::RAYWHITE;

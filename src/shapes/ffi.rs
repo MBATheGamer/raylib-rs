cf10d49cf0d540c079dd5c7ac37d97a68b916be4
MBATheGamer <mbathegamer@gmail.com>
2025-01-05 08:30:25 +0100

    Add `DrawLine` function inside `shapes/ffi` module

--- src/shapes/ffi.rs
+++ src/shapes/ffi.rs
@@ -2,6 +2,13 @@ use crate::structs::{Color, Rectangle};
 
 unsafe extern "C" {
   // Basic shapes drawing functions
+  pub unsafe fn DrawLine(
+    start_pos_x: i32,
+    start_pos_y: i32,
+    end_pos_x: i32,
+    end_pos_y: i32,
+    color: Color,
+  );
   pub unsafe fn DrawRectangle(pos_x: i32, pos_y: i32, width: i32, height: i32, color: Color);
   pub unsafe fn DrawRectangleRec(rectangle: Rectangle, color: Color);
 }

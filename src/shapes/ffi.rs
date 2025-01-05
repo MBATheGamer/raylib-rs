f03160a880582b067f123e8a3fba15cfaf475c6a
MBATheGamer <mbathegamer@gmail.com>
2025-01-05 08:29:42 +0100

    Add `DrawRectangleRec` function inside `shapes/ffi` module

--- src/shapes/ffi.rs
+++ src/shapes/ffi.rs
@@ -1,5 +1,7 @@
-use crate::structs::Color;
+use crate::structs::{Color, Rectangle};
 
 unsafe extern "C" {
+  // Basic shapes drawing functions
   pub unsafe fn DrawRectangle(pos_x: i32, pos_y: i32, width: i32, height: i32, color: Color);
+  pub unsafe fn DrawRectangleRec(rectangle: Rectangle, color: Color);
 }

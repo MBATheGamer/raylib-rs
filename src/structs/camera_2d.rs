88b8cd2ae38bc1fbfe73db450565f9d0595e5076
MBATheGamer <mbathegamer@gmail.com>
2025-01-05 06:42:04 +0100

    Create `Camera2D` struct inside `structs/camera_2d` module

---
+++ src/structs/camera_2d.rs
@@ -0,0 +1,8 @@
+use super::vector2::Vector2;
+
+pub struct Camera2D {
+  pub offset: Vector2,
+  pub target: Vector2,
+  pub rotation: f32,
+  pub zoom: f32,
+}
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -3,5 +3,7 @@ mod color;
 mod rectangle;
 mod vector2;
 
+pub use camera_2d::Camera2D;
 pub use color::Color;
 pub use rectangle::Rectangle;
+pub use vector2::Vector2;

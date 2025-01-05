3dd2da54bbac6e9d6a8ef5721e5d1f562359ef6b
MBATheGamer <mbathegamer@gmail.com>
2025-01-05 08:41:32 +0100

    Create `draw_line` function inside `shapes/draw/basic` module

--- src/shapes/draw/basic.rs
+++ src/shapes/draw/basic.rs
@@ -1,7 +1,14 @@
 use crate::{
-  shapes::ffi::{DrawRectangle, DrawRectangleRec},
+  shapes::ffi::{DrawLine, DrawRectangle, DrawRectangleRec},
   structs::{Color, Rectangle},
 };
+
+#[inline]
+pub fn draw_line(start_pos_x: i32, start_pos_y: i32, end_pos_x: i32, end_pos_y: i32, color: Color) {
+  unsafe {
+    DrawLine(start_pos_x, start_pos_y, end_pos_x, end_pos_y, color);
+  }
+}
 
 #[inline]
 pub fn draw_rectangle(pos_x: i32, pos_y: i32, width: i32, height: i32, color: Color) {
